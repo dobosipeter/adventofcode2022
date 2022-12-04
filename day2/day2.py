e3e8bf5ac2924da4b12ef4c72a6e95decea72e3c
DOP3BP <PeterLaszlo.Dobosi@hu.bosch.com>
2022-12-04 16:58:56 +0100

    Working on data wrangling methods.

--- day2/day2.py
+++ day2/day2.py
@@ -1,9 +1,9 @@
 from enum import Enum
 
 
-def day2(input_path: str) -> None:
+def solve(input_path: str) -> None:
     """
-    Calculate the score of the game.
+    Calculate the total score of the games.
     :param input_path: The path of the input file, containing the moves made by the players
     :return: Nothing, prints the final score.
     """
@@ -60,23 +60,54 @@ def day2(input_path: str) -> None:
                         return MoveScores.PAPER.value + OutcomeScores.WIN.value
                     case SecondMoves.SCISSORS:
                         return MoveScores.SCISSORS.value + OutcomeScores.LOST.value
+            # Check the first players move
             case FirstMoves.PAPER:
                 # Check the second players move
                 match second_move:
                     case SecondMoves.ROCK:
-                        pass
+                        return MoveScores.ROCK.value + OutcomeScores.LOST.value
                     case SecondMoves.PAPER:
-                        pass
+                        return MoveScores.PAPER.value + OutcomeScores.DRAW.value
                     case SecondMoves.SCISSORS:
-                        pass
+                        return MoveScores.SCISSORS.value + OutcomeScores.WIN.value
             case FirstMoves.SCISSORS:
                 # Check the second players move
                 match second_move:
                     case SecondMoves.ROCK:
-                        pass
+                        return MoveScores.ROCK.value + OutcomeScores.WIN.value
                     case SecondMoves.PAPER:
-                        pass
+                        return MoveScores.PAPER.value + OutcomeScores.LOST.value
                     case SecondMoves.SCISSORS:
-                        pass
+                        return MoveScores.SCISSORS.value + OutcomeScores.DRAW.value
 
+    def read_input(file_path: str) -> list:
+        """
+        Reads the input from the file.
 
+        :param file_path: The path of the file containing the moves to read.
+        :return: The moves in a nested list.
+        """
+        moves = []
+        # Read the lines
+        file = open(file_path)
+        lines = file.read().splitlines()
+        # Convert them into a format that we can handle
+        for line in lines:
+            moves.append(line.split(" "))
+        return moves
+
+    def convert_moves(moves: list) -> list:
+        """
+        Convert moves from strings to the move enums format.
+
+        :param moves: The move strings to convert.
+        :return: The converted moves.
+        """
+        # Iterate through the moves
+        for move_pairs in moves:
+            for move in move_pairs:
+                pass
+
+    # Do the actual calculation
+    moves_as_strings = read_input(input_path)
+    print(moves_as_strings)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from day1 import day1
+from day2 import day2
 
 if __name__ == '__main__':
-    day1.solve("day1/input.txt")
+    # day1.solve("day1/input.txt")
+    day2.solve("day2/input.txt")
